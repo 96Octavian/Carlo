1babb921646abdd1bd106786b72eae48872fd6d1
octavian <gogol.logog@hotmail.it>
2018-08-11 20:15:49 +0200

    Bozza per Divisibile

---
+++ Divisibile/Origine.c
@@ -0,0 +1,32 @@
+/* Controlla se una sequenza di numeri inseriti, terminante con 0, ha più di due divisori in 
+ * 2, 3, 5, 7, 11
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../useful.h"
+
+int main(void) {
+	int num, num_divisori;
+
+	printf("Inserisci un numero: ");
+	scanf("%d", &num);
+	while (num) { // In C 0 significa falso, quindi ogni altro valore di num va bene per il nostro ciclo
+		num_divisori = 0;
+
+		if (num % 2 == 0) num_divisori++;
+		if (num % 3 == 0) num_divisori++;
+		if (num % 5 == 0) num_divisori++;
+		if (num % 7 == 0) num_divisori++;
+		if (num % 11 == 0) num_divisori++;
+		// ...
+
+		if (num_divisori >= 2) printf("%d ha almeno due divisori\n", num);
+		else if (num_divisori == 1) printf("%d ha un solo divisore\n", num);
+		else printf("%d non ha divisori tra quelli scelti\n", num);
+
+		printf("Inserisci un numero: ");
+		scanf("%d", &num);
+	}
+	return 0;
+}
